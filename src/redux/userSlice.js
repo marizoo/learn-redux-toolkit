9b46be6ac5452d99dfb5a5d78ed989da436c0778
marizoo <marizoo@meow.woof>
2022-01-05 14:59:00 +0700

    initial start

---
+++ src/redux/userSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    name: "john",
+    email: "john@gmail.com",
+  },
+  reducers: {
+    update: (state, action) => {},
+  },
+});
